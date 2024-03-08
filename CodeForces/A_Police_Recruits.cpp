b0dbd5509f2308b2a0ad8c5052c4a2452f5d3854
Abhishek Meena <118628637+abhishekmeena33@users.noreply.github.com>
2024-03-08 23:38:38 +0530

    Hehe

---
+++ CodeForces/A_Police_Recruits.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <string>
+#include <set>
+using namespace std;
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long
+#define vec vector<ll>
+#define pb push_back
+#define vecpair vector <pair<ll ,ll>>
+#define mp make_pair
+#define map map<ll,ll>
+#define dd double
+#define bb bool
+#define ff false
+#define tt true
+#define all(x) x.begin(), x.end()
+#define scan(x,n) for(ll i = 0; i < n; i++) cin >> x[i];
+
+int digit_sum(int n) {
+    int sum = 0;
+    while (n>0) {
+        sum+=n%10;
+        n=n/10;
+    }
+    return sum;
+}
+/*===========================================================================================*/
+
+void solve(){
+    ll n;
+    cin >> n;
+    
+    ll police = 0,crime=0;
+    while(n--){
+        ll x;
+        cin >> x;
+        if(x>0) police+=x;
+        else if(x<0 && police>0) police--;
+        else crime++;
+    }
+    cout << crime;
+}
+
+int main(){
+    FAST
+    solve();
+    return 0;
+}
